e56bcb89f8cba7eab1064aece1e3a7cca30daa15
PhamTienDat <tdat9663@gmail.com>
2025-09-02 17:36:40 +0700

    Bài 57: Thêm model Device và Hướng dẫn migrate  & migrate react mail

--- src/shared/services/email.service.tsx
+++ src/shared/services/email.service.tsx
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { Resend } from 'resend'
 import envConfig from '../config'
-
+import OTPEmail from 'emails/otp'
 @Injectable()
 export class EmailService {
   private resend: Resend
@@ -9,12 +9,12 @@ export class EmailService {
     this.resend = new Resend(envConfig.RESEND_API_KEY)
   }
   sendOTP(payload: { email: string; code: string }) {
+    const subject= 'Mã xác thực ECOM của bạn'
     return this.resend.emails.send({
       from: 'ECOM <no-reply@toiladat.online>',
       to: [payload.email],
-      subject: 'Mã xác thực ECOM của bạn',
-      text: `Mã OTP ECOM: ${payload.code}. Hiệu lực 5 phút. Nếu không phải bạn, hãy bỏ qua.`,
-      html: `<p>Xin chào,</p><p>Mã OTP ECOM của bạn là <b>${payload.code}</b> (hiệu lực 5 phút).</p><p>Nếu không phải bạn yêu cầu, xin bỏ qua.</p>`,
+      subject,
+      react: <OTPEmail otpCode={payload.code} title={subject}/>
     })
   }
 }
